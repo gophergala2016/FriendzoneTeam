8ffaf20542da43a5367325ef29291a74754cdf8e
Jorge Sierra <eva_01_2015@hotmail.com>
2016-01-24 05:28:13 -0600

    Performer V1
    i dunno, maybe this works

--- util/performers/performer.go
+++ util/performers/performer.go
@@ -10,29 +10,31 @@ import (
 	//    "net/http"
 	//    "net/url"
 	"encoding/json"
-	"github.com/ChimeraCoder/anaconda"
+	//	"github.com/ChimeraCoder/anaconda"
 	"github.com/creamdog/gonfig"
+	service "github.com/gophergala2016/FriendzoneTeam/services"
 	"os"
 	//    util "github.com/gophergala2016/FriendzoneTeam/util/dateformat"
 )
 
 //tipo
-type arrMessage []anaconda.DirectMessage
+//type arrMessage []anaconda.DirectMessage
+type arrMessage []service.Scheduler
 type comandos struct {
 	comando       string
 	uso           string
 	instrucciones []string
 }
-
 type arrComandos []comandos
 
 const ESPACIO = ""
 const NOESPACIOS = "[\\S]+"
-const NOMBRE_ARCHIVO = "[a-zA-z./_0-9-~]+|(\"[a-zA-z./_0-9-\\s~]+\")"
+const NOMBRE_ARCHIVO = "[a-zA-z./_0-9-~]+"
 
 var flag = false
 
-func getMessages(jsonS string) (mesages []anaconda.DirectMessage) {
+//func getMessages(jsonS string) (mesages []anaconda.DirectMessage) {
+func getMessages(jsonS string) (mesages []service.Scheduler) {
 	var mensajes arrMessage
 	data := []byte(jsonS)
 	json.Unmarshal(data, &mensajes)
@@ -43,11 +45,14 @@ func getMessages(jsonS string) (mesages []anaconda.DirectMessage) {
 	return
 }
 
-func processMessages(messages []anaconda.DirectMessage) {
+//func processMessages(messages []anaconda.DirectMessage) {
+func processMessages(messages []service.Scheduler) {
 
 	for i := 0; i < len(messages); i++ {
-		estatus, comando := interpretar(messages[i].Text)
+		//estatus, comando := interpretar(messages[i].Text)
+		estatus, comando := interpretar(messages[i].Command)
 		fmt.Println("----")
+		fmt.Println(messages[i].Command)
 		fmt.Println(estatus)
 		fmt.Println(comando)
 		fmt.Println("----")
@@ -71,37 +76,126 @@ func interpretar(comando string) (status bool, ssh string) {
 	//var cli comandos
 
 	if err != nil {
-		fmt.Println("Se murio")
+		//fmt.Println("Se murio")
 		fmt.Println(err)
 	}
 
 	//fmt.Println(arrcadenas[0])
 	switch {
 	case arrcadenas[0] == "create":
-		if len(arrcadenas) == 2 && testRegexp(NOMBRE_ARCHIVO, arrcadenas[1]) {
-			str, _ = dicc.GetString("comandos/create/v1/comando", nil)
-			//fmt.Println("Exito")
-			//fmt.Println(str)
-			return true, completeRegExp(str, arrcadenas)
-		} else if len(arrcadenas) == 3 && testRegexp(NOMBRE_ARCHIVO, arrcadenas[1]) {
-			str, _ = dicc.GetString("comandos/create/v2/comando", nil)
-			//fmt.Println("Exito")
-			//fmt.Println(str)
-			return true, completeRegExp(str, arrcadenas)
-		} else {
-			return false, ""
-			//fmt.Println("MAL")
-		}
+		str = create(dicc, arrcadenas)
+	case arrcadenas[0] == "delete":
+		str = delete(dicc, arrcadenas)
+	case arrcadenas[0] == "move":
+		str = move(dicc, arrcadenas)
+	case arrcadenas[0] == "rename":
+		str = rename(dicc, arrcadenas)
+	case arrcadenas[0] == "server":
+		str = server(dicc, arrcadenas)
+	case strings.HasPrefix(arrcadenas[0], ":"):
+		str = custom(arrcadenas)
 	}
 
-	//fmt.Println(str)
+	if str != "" {
+		return true, str
+	}
+	return false, str
+}
 
-	//	for i := 0; i < len(arrcadenas); i++ {
-	//		fmt.Println(arrcadenas[i])
-	//	}
-	//comment
+func custom(arrcadenas []string) (comando string) {
+	var str string
+	str = strings.Join(arrcadenas, " ")
+	str = strings.TrimPrefix(str, ":")
+	return str
+}
 
-	return false, ""
+func server(dicc gonfig.Gonfig, arrcadenas []string) (comando string) {
+	var str string
+	switch {
+	case arrcadenas[1] == "new":
+		switch {
+		case arrcadenas[2] == "go":
+			str, _ = dicc.GetString("comandos/server/new/go/comando", nil)
+		case arrcadenas[2] == "lamp":
+			str, _ = dicc.GetString("comandos/server/new/lamp/comando", nil)
+		case arrcadenas[2] == "lemp":
+			str, _ = dicc.GetString("comandos/server/new/lemp/comando", nil)
+		case arrcadenas[2] == "mean":
+			str, _ = dicc.GetString("comandos/server/new/mean/comando", nil)
+		}
+
+	case arrcadenas[1] == "start":
+		str, _ = dicc.GetString("comandos/server/start/comando", nil)
+	case arrcadenas[1] == "restart":
+		str, _ = dicc.GetString("comandos/server/restart/comando", nil)
+	case arrcadenas[1] == "stop":
+		str, _ = dicc.GetString("comandos/server/stop/comando", nil)
+	}
+
+	return completeRegExp(str, arrcadenas)
+}
+
+func rename(dicc gonfig.Gonfig, arrcadenas []string) (comando string) {
+	var str string
+	if testRegexp(NOMBRE_ARCHIVO, arrcadenas[1]) {
+		if strings.HasSuffix(arrcadenas[1], "/") {
+			str, _ = dicc.GetString("comandos/rename/v2/comando", nil)
+		} else {
+			str, _ = dicc.GetString("comandos/rename/v1/comando", nil)
+		}
+		return completeRegExp(str, arrcadenas)
+	} else {
+		return ""
+	}
+}
+
+func move(dicc gonfig.Gonfig, arrcadenas []string) (comando string) {
+	var str string
+	if testRegexp(NOMBRE_ARCHIVO, arrcadenas[1]) {
+		if strings.HasSuffix(arrcadenas[1], "/") {
+			str, _ = dicc.GetString("comandos/move/v2/comando", nil)
+		} else {
+			str, _ = dicc.GetString("comandos/move/v1/comando", nil)
+		}
+		return completeRegExp(str, arrcadenas)
+	} else {
+		return ""
+	}
+}
+
+func delete(dicc gonfig.Gonfig, arrcadenas []string) (comando string) {
+	var str string
+	if testRegexp(NOMBRE_ARCHIVO, arrcadenas[1]) {
+		if strings.HasSuffix(arrcadenas[1], "/") {
+			str, _ = dicc.GetString("comandos/delete/v2/comando", nil)
+		} else {
+			str, _ = dicc.GetString("comandos/delete/v1/comando", nil)
+		}
+		return completeRegExp(str, arrcadenas)
+	} else {
+		return ""
+	}
+}
+
+func create(dicc gonfig.Gonfig, arrcadenas []string) (comando string) {
+	var str string
+	if len(arrcadenas) == 2 && testRegexp(NOMBRE_ARCHIVO, arrcadenas[1]) {
+		if strings.HasSuffix(arrcadenas[1], "/") {
+			str, _ = dicc.GetString("comandos/create/v3/comando", nil)
+		} else {
+			str, _ = dicc.GetString("comandos/create/v1/comando", nil)
+		}
+		return completeRegExp(str, arrcadenas)
+	} else if len(arrcadenas) == 3 && testRegexp(NOMBRE_ARCHIVO, arrcadenas[1]) {
+		if strings.HasSuffix(arrcadenas[1], "/") {
+			str, _ = dicc.GetString("comandos/create/v4/comando", nil)
+		} else {
+			str, _ = dicc.GetString("comandos/create/v2/comando", nil)
+		}
+		return completeRegExp(str, arrcadenas)
+	} else {
+		return ""
+	}
 }
 
 func testRegexp(exp, val string) (valor bool) {
@@ -112,17 +206,17 @@ func testRegexp(exp, val string) (valor bool) {
 
 func completeRegExp(ssh string, param []string) (truessh string) {
 	var tssh string
-	if strings.Contains(ssh, "$2") && len(param) >= 3 {
+
+	if len(param) >= 3 {
 		//Si hay un placeholder %2, remplaza ese
 		//fmt.Println("ok")
-		tssh = strings.Replace(ssh, "$1", param[2], 1)
-		tssh = strings.Replace(tssh, "$2", param[1], 1)
+		tssh = strings.Replace(ssh, "$1", param[2], -1)
+		tssh = strings.Replace(tssh, "$2", param[1], -1)
 		tssh = strings.Replace(tssh, "//", "/", -1)
 	} else {
-		tssh = strings.Replace(tssh, "$1", param[1], 1)
+		tssh = strings.Replace(ssh, "$1", param[1], -1)
 		tssh = strings.Replace(tssh, "//", "/", -1)
 	}
-
 	/*
 		fmt.Println("---")
 		fmt.Println(strings.Contains(ssh, "$2"))
