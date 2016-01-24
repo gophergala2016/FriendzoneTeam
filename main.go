73cc4331818648f7b315a2ee5dd16a5e744bfd0d
Paul Arizpe Colorado <kiramishima@outlook.com>
2016-01-23 18:51:42 -0600

    PEAC: Modificacion

--- main.go
+++ main.go
@@ -1,46 +1,11 @@
 package main
 
 import ( 
-    "fmt"
-    "time"
     "net/http"
-    "net/url"
-    "encoding/json"
-    "github.com/ChimeraCoder/anaconda"
-    util "github.com/gophergala2016/FriendzoneTeam/util/dateformat"
+    service "github.com/gophergala2016/FriendzoneTeam/services"
 )
 
 func main()  {
-    http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request){
-        // Verifica si ha obtenido nuevos DMs
-        anaconda.SetConsumerKey("qFwSZ5VTSw2E23u166iWPAZj1")
-        anaconda.SetConsumerSecret("AgJPsgKrP1OW3fx9TpDwZ55beUMGdBLcybJLyUvgMJM0tQj5PD")
-        api := anaconda.NewTwitterApi("151138456-QnyorQfe59zld78ATJdfwNpcpsywVPZqcqo5uXiZ", "TpG0upWDAAmXF81K0HscQOxXCqHfpWEYd66c7fVdLX1k8")
-        // var dms []anaconda.DirectMessage
-        uri := url.Values{}
-        if api.Credentials == nil {
-            fmt.Println("Twitter Api client has empty (nil) credentials")
-        }
-        searchResult, _ := api.GetDirectMessages(uri)
-        output, err := json.Marshal(searchResult)
-        
-        if err != nil {
-            fmt.Println("Error parseando JSON :c")
-        }
-        
-        // Envio DMs
-        /* message, _ := api.PostDMToScreenName("chemas sin json", "Chemasmas")
-        if err != nil {
-            fmt.Println("Error enviando el DM :c")
-        }
-        fmt.Println(message)*/
-        currentDate := time.Now()
-        fmt.Printf("%d-%s-%d\n", currentDate.Day(), currentDate.Month(), currentDate.Year())
-        strdate, _ := util.DateFormat("Sat Jan 23 19:46:10 +0000 2016")
-        fmt.Println(strdate)
-        
-        fmt.Fprintf(w, string(output))
-    })
-    
+    http.HandleFunc("/test", service.RevisarDM)
     http.ListenAndServe(":3000", nil)
 }
--- services/services.go
+++ services/services.go
@@ -1,33 +1,57 @@
 package service
+
 import (
     "fmt"
     "log"
     "time"
     "net/http"
     "net/url"
+    "encoding/json"
     "github.com/ChimeraCoder/anaconda"
+    util "github.com/gophergala2016/FriendzoneTeam/util/dateformat"
 )
 
-// Revisa DMs en base a la fecha actual
+// Regresa los DMs que se han recibido en base a la fecha actual
 func RevisarDM(w http.ResponseWriter, r *http.Request){
     // Verifica si ha obtenido nuevos DMs
-    anaconda.SetConsumerKey("qFwSZ5VTSw2E23u166iWPAZj1")
-    anaconda.SetConsumerSecret("AgJPsgKrP1OW3fx9TpDwZ55beUMGdBLcybJLyUvgMJM0tQj5PD")
-    api := anaconda.NewTwitterApi("151138456-QnyorQfe59zld78ATJdfwNpcpsywVPZqcqo5uXiZ", "TpG0upWDAAmXF81K0HscQOxXCqHfpWEYd66c7fVdLX1k8")
-    // Revisamos que ejecute bien la conexion con las credenciales
+    anaconda.SetConsumerKey("lCqA4GsOhivuJumCMynVuOI2B")
+    anaconda.SetConsumerSecret("B3XOc0n1FLw0faxl2SSCC7xNFxAAWdPnL7shzLj0Sq3l3OUqvE")
+    api := anaconda.NewTwitterApi("4804703832-9fNj7vcJBobyDdbYhDPKOfFbTJzXkq64VFr99Qr", "aK3dWB3HoM01p79UFRTD8eQh9SYCwElr1RCicqe3imHBf")// Revisamos que ejecute bien la conexion con las credenciales
     if api.Credentials == nil {
-        fmt.Println("Twitter Api client has empty (nil) credentials")
+        log.Println("Twitter Api client has empty (nil) credentials")
     }
     uri := url.Values{}
     dmResults, err := api.GetDirectMessages(uri)
     if err != nil {
         log.Printf("Error: %s", err.Error())
     }
-    currentDate := time.Now()
-    fmt.Printf(currentDate.Format("dd mm yyyy"))
-    for _, message := range dmResults {
-        fmt.Println(message.CreatedAt)
+    
+    output, err := json.Marshal(dmResults)
+    if err != nil {
+        log.Printf("Error: %s", err.Error())
     }
     
-    fmt.Fprintf(w, "hi")
+    cd := time.Now()
+    currentDate := fmt.Sprintf("%d-%s-%d\n", cd.Day(), cd.Month(), cd.Year())
+    i := 0
+    for _, message := range dmResults {
+        strFormat, err := util.DateFormat(message.CreatedAt)
+        if err != nil {
+            log.Fatal(err.Error())
+        }
+        if currentDate == strFormat {
+            i++
+        }
+        fmt.Printf("%d", i)
+    }
+    
+    fmt.Fprintf(w, string(output))
+}
+
+func createupdate() {
+    
+}
+
+func Command(){
+    
 }
